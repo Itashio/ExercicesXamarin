b7d59e798aa9b56a0ef093505fe381af696e1181
Itashio <53567197+Itashio@users.noreply.github.com>
2019-11-03 18:20:16 +0100

    01

---
+++ Exericices/Exericices/Exercice1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exericices
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercice1 : ContentPage
+    {
+        public Exercice1()
+        {
+            InitializeComponent();
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                Padding = new Thickness(20, 30, 20, 20);
+            }
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                Padding = new Thickness(20, 40,20, 20);
+            }
+
+        }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            displaySize.FontSize = value;
+            displayLabel.Text = String.Format("Font Size: {0}", value);
+        }
+
+    }
+}
---
+++ Exericices/Exericices/ImagesStackLayoutExercice.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exericices
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagesStackLayoutExercice : ContentPage
+    {
+        public ImagesStackLayoutExercice()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
